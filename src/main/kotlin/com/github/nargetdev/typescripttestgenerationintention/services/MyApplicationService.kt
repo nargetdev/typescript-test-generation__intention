8b8284fa9f5f64864eddd0cebf42898323319dd8
GitHub Action <action@github.com>
2020-07-17 23:09:56 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/nargetdev/typescripttestgenerationintention/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nargetdev.typescripttestgenerationintention.services
+
+import com.github.nargetdev.typescripttestgenerationintention.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
